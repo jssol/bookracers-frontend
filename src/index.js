3e3fced50e18b6e2f95a0ce1024bf52ed03cd282
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-15 14:22:33 +0200

    Setup basic Redux ducks pattern

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
