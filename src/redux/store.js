cf94f4e58a6a9bd2e417a980017eee0b54e418b4
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-18 23:48:49 +0200

    Add logger middleware to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import authReducer from './authentication/authentication';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
