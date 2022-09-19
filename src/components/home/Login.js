ca537afa0b90692d79c3a0e8ce2409ccbe81ddaf
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-19 02:27:39 +0200

    Config index.js as of React 18

--- src/components/home/Login.js
+++ src/components/home/Login.js
@@ -1,14 +1,49 @@
+import React from 'react';
+import { useFormik } from 'formik';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { NavLink, useNavigate } from 'react-router-dom';
 import '../assets/styles/Login.scss';
 
-const Login = () => (
-  <div className="login">
-    <h3>Login</h3>
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button className="btn" type="submit">Login</button>
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify({ user: values }, null, 2));
+    },
+  });
+
+  return (
+    <form
+      className="form-container"
+      onSubmit={formik.handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        className="form-input"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        className="form-input"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+      />
+      <button
+        type="submit"
+        className="button"
+      >
+        Login
+      </button>
     </form>
-  </div>
-);
+  );
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 import './index.scss';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 );
