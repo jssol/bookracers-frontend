e90b1d50e61c05ed59d64273b12a46a98eef1319
[Dinma-Faith] <[phaytep@gmail.com]>
2022-09-21 16:15:30 +0100

    Add classNames to component

--- src/components/motorcycles/MotorcycleCard.js
+++ src/components/motorcycles/MotorcycleCard.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { nanoid } from '@reduxjs/toolkit';
 import PropTypes from 'prop-types';
+import {
+  FaFacebook, FaInstagram, FaTwitter, FaWhatsapp,
+} from 'react-icons/fa';
 
 function MotorcycleCard({ motor }) {
   const params = useParams();
@@ -10,32 +13,45 @@ function MotorcycleCard({ motor }) {
 
   return (
     <>
-      <h2>
-        Category Name:
-        {catname}
-      </h2>
-      <h2>
-        Category ID:
-        {id}
-      </h2>
+      <h2 className="model-header">LATEST MODELS</h2>
+      <br />
+      <p className="model-header modelheader-ptag">Check out the latest models from our partners</p>
+
+      <div className="model-name">
+        <p>
+          {catname}
+        </p>
+        <p>
+          {id}
+        </p>
+      </div>
       {motorcycles.length
         ? motorcycles.map((el) => (
           <Link to={`/categories/${params.id}/motorcycles/${id}`} key={nanoid()}>
-            <div className="card">
+            <div className="card card-content">
+              <div className="img-div">
+                { el.image && (<img src={el.image} alt="" className="imgSize" />) }
+              </div>
               <ul>
-                <li>
+                <li className="model">
                   Motorcycle Model:
                   {' '}
                   {el.model}
                 </li>
-                <li>
+                <li className="content-id">
                   ID:
                   {' '}
                   {el.id}
                 </li>
               </ul>
-              { el.image && (<img src={el.image} alt="" className="imgSize" />) }
+              <ul className="socials">
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaTwitter /></li>
+                <li><FaWhatsapp /></li>
+              </ul>
             </div>
+
           </Link>
         ))
         : null}
