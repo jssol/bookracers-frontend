7a5c73b0e47b10d2b90f7a2efacf6388185a258d
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-25 12:24:42 +0500

    Create logout user functionality

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import signupReducer from './user/signupSlice';
 import loginReducer from './user/loginSlice';
+import logoutReducer from './user/logoutSlice';
 
 const logger = createLogger();
 
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     signup: signupReducer,
     login: loginReducer,
+    logout: logoutReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
