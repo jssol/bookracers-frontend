b8dd16603cf771fa1110c53534a673f60789bd84
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-26 16:23:56 +0200

    rename date hadler variable

--- src/helpers/dateHandler.js
+++ src/helpers/dateHandler.js
@@ -1,8 +1,8 @@
 const totalPrice = (startDate, endDate) => {
   const start = new Date(startDate);
   const end = new Date(endDate);
-  const difference = end.getTime() - start.getTime();
-  const days = Math.ceil(difference / (1000 * 3600 * 24));
+  const diff = end.getTime() - start.getTime();
+  const days = Math.ceil(diff / (1000 * 3600 * 24));
   return days;
 };
 
