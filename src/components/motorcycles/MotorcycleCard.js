8660aa23f31249311d676dcc794f4b534ebc3b5b
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-28 21:34:43 +0200

    Render images

--- src/components/motorcycles/MotorcycleCard.js
+++ src/components/motorcycles/MotorcycleCard.js
@@ -30,7 +30,7 @@ function MotorcycleCard({ motor }) {
             <Link to={`/categories/${params.id}/motorcycles/${el.id}`} key={nanoid()}>
               <div className="card card-content">
                 <div className="img-div">
-                  { el.image && (<img src={el.image} alt="" className="imgSize" />) }
+                  { el.picture ? (<img src={`http://localhost:3001/${el.picture}`} alt="" className="imgSize" />) : (<img src={el.image} alt="" className="imgSize" />)}
                 </div>
                 <ul>
                   <li className="model">
