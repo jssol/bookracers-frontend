5bf7a7ee28c46bfe4c4377b876a7ad474185ae2f
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-28 13:11:57 +0200

    Add motorcycle service

---
+++ src/redux/motorcycle/motorcyle.service.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const newotorcycle = createAsyncThunk(
+  'createmotorcycle/createmotorcycle',
+  async (resource) => {
+    const response = await axios.post('http://localhost:3001/api/v1/add_motorcycle', { motorcycle:resource }, {
+      headers: {
+        Authorization: `${localStorage.getItem('token')}`,
+      },
+    });
+    const res = await response.data;
+    return res;
+  },
+);
+
+export default newotorcycle;
