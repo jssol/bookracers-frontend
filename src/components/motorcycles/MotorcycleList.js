41086e66d0e36dc3549217f8f6c1e7579fca02f4
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-29 09:48:50 +0500

    Incorporate motorcycle feature branch

--- src/components/motorcycles/MotorcycleList.js
+++ src/components/motorcycles/MotorcycleList.js
@@ -46,11 +46,15 @@ function MotorcycleList() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (model === '' || year === '' || brand === '' || picture === '' || rentalPrice === '') {
+    if (
+      model === ''
+      || year === ''
+      || brand === ''
+      || picture === ''
+      || rentalPrice === ''
+    ) {
       setMessage('Please fill all the fields');
     } else {
-      // imge upload
-      // setReserved(false);
       const motorData = new FormData();
       motorData.append('motorcycle[picture]', picture);
       motorData.append('motorcycle[model]', model);
@@ -71,94 +75,120 @@ function MotorcycleList() {
         <Navbar />
         <Toggle />
       </div>
-      {message === 'Motorcycle created successfully' ? (
-        <div className="alert alert-success" role="alert">
-          {message}
+      <div className="motorlist-container">
+        <div className="motorlist-header">
+          {message === 'Motorcycle created successfully' ? (
+            <div className="alert alert-success" role="alert">
+              {message}
+            </div>
+          ) : (
+            <div className="alert alert-danger" role="alert">
+              {message}
+            </div>
+          )}
+          <div className="motorcycle-list-header-container">
+            <h2 className="model-header m-header">LATEST MODELS</h2>
+            <p className="model-header modelheader-ptag">
+              Check out the latest models from our partners
+            </p>
+            <div className="model-name">
+              <p>
+                Category
+                {' '}
+                {params.id}
+                {' '}
+                {catname}
+              </p>
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className="alert alert-danger" role="alert">
-          {message}
-        </div>
-      )}
-      <div>
-        {localStorage.getItem('isAdmin') === 'true'
-          ? (
+        <div>
+          {localStorage.getItem('isAdmin') === 'true' ? (
             <button type="button" className="addMotorBtn" onClick={handleShow}>
               <strong>+</strong>
               Add Motorcycle
             </button>
           ) : null}
-        {motorcycles.length
-          ? motorcycles.map((motorcycle) => (
-            <MotorcycleCard key={nanoid()} motor={motorcycle} />
-          ))
-          : null}
-      </div>
-      <Modal show={show} onHide={handleClose} className="modal">
-        <Modal.Header>
-          <div className="addMotorBtn">
-            <CloseButton variant="white" />
-          </div>
-          <Modal.Title><h2>Add New Motocycle</h2></Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="modal-content">
-          <form className="form-container" onSubmit={submitHandler}>
-            <input
-              type="text"
-              placeholder="Brand"
-              name="brand"
-              className="form-input"
-              value={brand}
-              onChange={(e) => setBrand(e.target.value)}
-              required
-            />
-            <input
-              type="text"
-              placeholder="Model"
-              name="model"
-              className="form-input"
-              value={model}
-              onChange={(e) => setModel(e.target.value)}
-              required
-            />
+          {motorcycles.length
+            ? motorcycles.map((motorcycle) => (
+              <MotorcycleCard key={nanoid()} motor={motorcycle} />
+            ))
+            : null}
+        </div>
 
-            <DatePicker
-              selected={year}
-              onChange={(year) => setYear(year)}
-              name="year"
-              className="form-input"
-              showYearPicker
-              dateFormat="yyyy"
-              required
-            />
-            <input
-              type="number"
-              placeholder="Rental Price"
-              name="rental_price"
-              className="form-input"
-              value={rentalPrice}
-              onChange={(e) => setRentalPrice(e.target.value)}
-              min="0"
-              required
-            />
-            <input
-              type="file"
-              name="image"
-              className="form-input"
-              onChange={(e) => setPicture(e.target.files[0])}
-              required
-            />
-            <Button
-              type="submit"
-              variant="success"
-              onClick={handleClose}
-              className="form-button button"
-            >
-              Add Motorcycle
-            </Button>
-          </form>
-        </Modal.Body>
-      </Modal>
+        {/* <div className="motorcycle-list">
+
+        <div> */}
+
+        <Modal show={show} onHide={handleClose} className="modal">
+          <Modal.Header>
+            <div className="addMotorBtn">
+              <CloseButton variant="white" />
+            </div>
+            <Modal.Title>
+              <h2>Add New Motocycle</h2>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="modal-content">
+            <form className="form-container" onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Brand"
+                name="brand"
+                className="form-input"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Model"
+                name="model"
+                className="form-input"
+                value={model}
+                onChange={(e) => setModel(e.target.value)}
+                required
+              />
+
+              <DatePicker
+                selected={year}
+                onChange={(year) => setYear(year)}
+                name="year"
+                className="form-input"
+                showYearPicker
+                dateFormat="yyyy"
+                required
+              />
+              <input
+                type="number"
+                placeholder="Rental Price"
+                name="rental_price"
+                className="form-input"
+                value={rentalPrice}
+                onChange={(e) => setRentalPrice(e.target.value)}
+                min="0"
+                required
+              />
+              <input
+                type="file"
+                name="image"
+                className="form-input"
+                onChange={(e) => setPicture(e.target.files[0])}
+                required
+              />
+              <Button
+                type="submit"
+                variant="success"
+                onClick={handleClose}
+                className="form-button button"
+              >
+                Add Motorcycle
+              </Button>
+            </form>
+          </Modal.Body>
+        </Modal>
+
+      </div>
     </div>
   );
 }
