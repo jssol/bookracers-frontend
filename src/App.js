de1e45309c846c4179ca7e2f82a1e39d59a6a8b8
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-30 11:39:29 +0200

    Remove unused routes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import NewSignUp from './components/home/NewSignUp';
 import CategoriesPage from './components/categories/CategoriesPage';
 import MotorcycleList from './components/motorcycles/MotorcycleList';
 import MotorcycleDetails from './components/motorcycles/MotorcycleDetails';
-import Navbar from './components/navigation/Navbar';
-import Toggle from './components/navigation/Toggle';
 import AddCategory from './components/crud/AddCategory';
 import MyReservations from './components/reservations/MyReservations';
 import Reservation from './components/reservations/reservation';
@@ -31,9 +29,6 @@ const App = () => (
         path="/categories/:id/motorcycles/:mid"
         element={<MotorcycleDetails />}
       />
-
-      <Route path="*" element={<Navbar />} />
-      <Route path="/toggle" element={<Toggle />} />
       <Route path="/add_category" element={<AddCategory />} />
       <Route path="/my_reservations" element={<MyReservations />} />
       <Route path="/categories/:id/motorcycles/:mid/reservation" element={<Reservation />} />
